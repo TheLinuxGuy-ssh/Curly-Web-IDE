150b71a5076dfc5da4ec8cd0bae8129f68b12502
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-14 11:30:12 +0530

    terminal  parameter fixed

--- src/components/ide/Topbar.jsx
+++ src/components/ide/Topbar.jsx
@@ -3,7 +3,7 @@ import EditorBtn from "../EditorBtn";
 
 const Topbar = () => {
     return (
-    <div className="topbar">
+    <div className="topbar flex justify-end p-1">
         <EditorBtn px="2">
             <i className="fa-regular fa-user"></i>
         </EditorBtn>
