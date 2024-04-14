606ccf5ff39a30520c4ee7f725b1c8e4fa71d77a
Aryan Sharma <76253876+Aryan110303@users.noreply.github.com>
2024-04-14 16:23:19 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import 'react-toastify/dist/ReactToastify.css';
 import Editor from "./Editor"
 
 const router = createBrowserRouter([
