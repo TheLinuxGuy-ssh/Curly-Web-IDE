44d38f3a2d4f632002bb3d77255f22320679fd07
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-14 16:14:15 +0530

    syntax

--- src/components/ide/Sidebar.jsx
+++ src/components/ide/Sidebar.jsx
@@ -2,21 +2,22 @@ import { Resizable } from 're-resizable';
 
 const Sidebar = () => {
     return (
+
         <Resizable
-  defaultSize={{
-    width: "20%",
-    height: "100%",
-  }}
-  minWidth="8vh"
-  maxWidth="25%"
-  enable={{
-    right: true
-  }}
->
-        <div className="sidebar w-[100%] h-screen bg-black">
-        sidebar
-    </div>
-    </Resizable>
+          defaultSize={{
+          width: "20%",
+          height: "100%",
+          }}
+          minWidth="8vh"
+          maxWidth="25%"
+          enable={{
+            right: true
+          }}>
+          <div className="sidebar w-[100%] h-screen bg-black">
+            sidebar
+          </div>
+        </Resizable>
+        
     )
 }
 
--- src/components/ide/Terminal.jsx
+++ src/components/ide/Terminal.jsx
@@ -2,20 +2,19 @@ import { Resizable } from 're-resizable';
 
 const Terminal = () => {
     return (
-        <Resizable
-  defaultSize={{
-    width: "100%",
-    height: "20%",
-  }}
-  minHeight="0"
-  maxHeight="40%"
-  enable={{
-    top: true
-  }}
->
-        <div className="terminal">
+        <Resizable 
+          defaultSize={{
+            width: "100%",
+            height: "20%",
+          }}
+          minHeight="0"
+          maxHeight="40%"
+          enable={{
+            top: true
+          }}>
+          <div className="terminal">
             Terminal
-        </div>
+          </div>
         </Resizable>
     )
 }
