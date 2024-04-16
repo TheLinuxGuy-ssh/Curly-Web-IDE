bdcc4c4bac7713cb57231eccb6edf2dd54a09784
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-16 23:47:06 +0530

    added felix the robot

--- src/components/ide/Code.jsx
+++ src/components/ide/Code.jsx
@@ -1,7 +1,15 @@
 const Code = () => {
     return (
         <div className="code">
-            code
+            <div id="felix" class="felix" onclick="activateFelix();">
+      <div class="eyes">
+        <div class="eye left"></div>
+        <div class="eye right"></div>
+      </div>
+      <div className="felix-body">
+        
+      </div>
+    </div>
         </div>
     )
 }
