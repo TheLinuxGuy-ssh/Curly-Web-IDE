383e35969734b5521fe36ef9e6b0bbb64882a5cf
TheLinuxGuy <107614947+TheLinuxGuy-ssh@users.noreply.github.com>
2025-02-23 14:02:27 +0000

    updated features

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -22,8 +22,8 @@ const Who = () => {
     <img src="https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="Big Ben" />
        
     <div className="card__overlay">
-      <h2>London</h2>
-      <p>Big Ben</p>
+      <h2>200+</h2>
+      <p>VSCode Extensions Support</p>
     </div>
     </div>
   </div>
@@ -32,8 +32,8 @@ const Who = () => {
     <img src="https://images.unsplash.com/photo-1581010864468-c972b8705439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Eiffel Tower" />
           
     <div className="card__overlay">
-      <h2>Paris</h2>
-      <p>Eiffel Tower</p>
+      <h2>Universal Support</h2>
+      <p>Includng Phones</p>
     </div>
     </div>
   </div>
@@ -42,8 +42,8 @@ const Who = () => {
     <img src="https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1096&q=80" alt="Colosseum" />
     
     <div className="card__overlay">
-      <h2>Rome</h2>
-      <p>Colosseum</p>
+      <h2>Fully Secure Authentication</h2>
+      <p>Focused towards privacy</p>
     </div>
     </div>
   </div>
@@ -51,8 +51,8 @@ const Who = () => {
   <div className="card__img">  
     <img src="https://images.unsplash.com/photo-1581473483413-313a5afffb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80" alt="Pisa Tower" />
     <div className="card__overlay">
-      <h2>Pisa</h2>
-      <p>Pisa Tower</p>
+      <h2>Git Support</h2>
+      <p>with full repository management</p>
     </div>
     </div>
   </div>
@@ -61,8 +61,8 @@ const Who = () => {
     <img src="https://images.unsplash.com/photo-1585155967849-91c736589c84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="" />
    
     <div className="card__overlay">
-      <h2>New York</h2>
-      <p>Statue of Liberty</p>
+      <h2>Effecient Base</h2>
+      {/* <p></p> */}
     </div>
     </div>
   </div>
@@ -71,8 +71,8 @@ const Who = () => {
     <img src="https://images.unsplash.com/photo-1527915676329-fd5ec8a12d4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="Sydney Opera House" />
  
     <div className="card__overlay">
-      <h2>Sydney</h2>
-      <p>Sydney Opera House</p>
+      <h2>Modern UI/UX for getting work done with performance</h2>
+      
     </div>
     </div>
   </div>
